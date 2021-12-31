a7048f47889d7972594e7b50e70ba99f86246294
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-12-31 13:59:59 +0530

    Added : Program for add the contact from console

--- src/com/bl/day9/AddressBook.java
+++ src/com/bl/day9/AddressBook.java
@@ -1,5 +1,7 @@
 package com.bl.day9;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
 	public static void main(String[] args) {
@@ -13,6 +15,8 @@ public class AddressBook {
 		newContact.setPhoneNumber("9822726196 \n");
 		newContact.setEmail("sandipankanade.sk@gmail.com \n");
 		System.out.println("The Contact is : \n" + newContact);
+		newAddressBook person = new newAddressBook();
+		person.addContact();
 
 	}
 
@@ -89,4 +93,37 @@ class contactDetails {
 
 	}
 
+}
+
+class newAddressBook {
+
+	Scanner sc = new Scanner(System.in);
+
+	public void addContact() {
+		contactDetails person = new contactDetails();
+		System.out.println("Enter First Name: ");
+		String firstName = sc.nextLine();
+		System.out.println("Enter last Name: ");
+		String lastName = sc.nextLine();
+		System.out.println("Enter your addressCity: ");
+		String addressCity = sc.nextLine();
+		System.out.println("Enter your state: ");
+		String state = sc.nextLine();
+		System.out.println("Enter zip code : ");
+		Long zip = sc.nextLong();
+		sc.nextLine();
+		System.out.println("Enter phone number: ");
+		String phoneNumber = sc.nextLine();
+		sc.nextLine();
+		System.out.println("Enter your EMail ID: ");
+		String email = sc.nextLine();
+		person.setFirstName(firstName);
+		person.setLastName(lastName);
+		person.setAddressCity(addressCity);
+		person.setState(state);
+		person.setZip(zip.intValue());
+		person.setPhoneNumber(phoneNumber);
+		person.setEmail(email);
+		System.out.println("The Contact Details of " + firstName + "\n" + person);
+	}
 }
